c664080691639e516b1ed12947f039a418b3e568
Llamalectric <lotsnlotsofllamas@gmail.com>
2024-10-19 23:04:03 -0700

    implement flood fill of colors, base game complete

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
---Magenta, red, green, blue, yellow, cyan
+-- Magenta, red, green, blue, yellow, cyan
 local pixelColors = { { 255, 0, 255 }, { 255, 0, 0 }, { 0, 255, 0 }, { 0, 0, 255 }, { 255, 255, 0 }, { 0, 255, 255 } }
 -- Line color
 local black = { 0, 0, 0 }
@@ -26,8 +26,9 @@ math.randomseed(os.time())
 -- Table of pixels
 local rectangles = {}
 for i = 1, numPixels do
+	table.insert(rectangles, {})
 	for j = 1, numPixels do
-		table.insert(rectangles, {
+		table.insert(rectangles[i], {
 			color = pixelColors[math.random(6)],
 			x = screenHeight / numPixels * (i - 1),
 			y = screenHeight / numPixels * (j - 1),
@@ -35,29 +36,67 @@ for i = 1, numPixels do
 	end
 end
 
-local colorPicked = rectangles[1].color
-
 function love.load()
 	love.window.setMode(screenWidth, screenHeight)
 	love.window.setTitle("Pixel painter 🦙🖌️")
-	love.graphics.setBackgroundColor({ 0, 0, 0 })
+	love.graphics.setBackgroundColor(black)
 end
 
-function love.mousereleased(x, y, button, istouch, presses)
+function love.mousereleased(x, y, button, _, _)
 	if x > screenHeight and button == 1 then
 		-- Figure out which color was picked
 		-- y / 180 -> y / (screenHeight / pixelColors.length)
 		-- floor   -> we don't care where in the box was clicked, chop off decimal
 		-- + 1     -> lua is not zero indexed :(
-		colorPicked = pixelColors[math.floor(y / 180) + 1]
+		local colorPicked = pixelColors[math.floor(y / 180) + 1]
+		Change_color(colorPicked)
+	end
+end
+
+-- https://en.wikipedia.org/wiki/Flood_fill#Stack-based_recursive_implementation_(four-way)
+function Change_color(colorTo)
+	local pixelsToChange = {}
+	table.insert(pixelsToChange, rectangles[1][1])
+	local colorFrom = rectangles[1][1].color
+	while next(pixelsToChange) ~= nil do
+		local p = table.remove(pixelsToChange, 1)
+		if p.color == colorFrom then
+			p.color = colorTo
+			local loc = FindPixel(p)
+			if loc.col ~= 1 then
+				table.insert(pixelsToChange, rectangles[loc.col - 1][loc.row])
+			end
+			if loc.col ~= numPixels then
+				table.insert(pixelsToChange, rectangles[loc.col + 1][loc.row])
+			end
+			if loc.row ~= 1 then
+				table.insert(pixelsToChange, rectangles[loc.col][loc.row - 1])
+			end
+			if loc.row ~= numPixels then
+				table.insert(pixelsToChange, rectangles[loc.col][loc.row + 1])
+			end
+		end
+	end
+end
+
+-- Optimize... O(n^2)
+function FindPixel(pixel)
+	for i, arr in ipairs(rectangles) do
+		for j, rect in ipairs(arr) do
+			if rect == pixel then
+				return { col = i, row = j }
+			end
+		end
 	end
 end
 
 function love.draw()
 	-- Draw pixels
-	for _, rect in pairs(rectangles) do
-		love.graphics.setColor(rect.color)
-		love.graphics.rectangle("fill", rect.x, rect.y, screenHeight / numPixels, screenHeight / numPixels)
+	for _, arr in pairs(rectangles) do
+		for _, rect in pairs(arr) do
+			love.graphics.setColor(rect.color)
+			love.graphics.rectangle("fill", rect.x, rect.y, screenHeight / numPixels, screenHeight / numPixels)
+		end
 	end
 	-- Draw color picker
 	for i = 1, #pixelColors do
